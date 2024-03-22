53a1f1238fa0c1acf402edb11be52a389ccb0aa4
BrunoFreitas <brunods.freitas@hotmail.com>
2024-03-21 23:52:32 -0300

    feat: added new modules

--- Semana-Conectividade-e-Redes/Desafio4/main.tf
+++ Semana-Conectividade-e-Redes/Desafio4/main.tf
@@ -4,7 +4,8 @@
 
 
 module "vpc" { #https://github.com/terraform-aws-modules/terraform-aws-vpc
-  source = "terraform-aws-modules/vpc/aws"
+  #source = "terraform-aws-modules/vpc/aws"
+  source = "./modules/vpc"
 
   name = "${var.desc_tags.project}-vpc"
   cidr = var.vpc_cidr_block
@@ -14,10 +15,10 @@ module "vpc" { #https://github.com/terraform-aws-modules/terraform-aws-vpc
   public_subnets  = var.public_subnets
   intra_subnets   = var.intra_subnets
 
-  default_vpc_enable_dns_support = true
-  default_vpc_enable_dns_hostnames = true
+  default_vpc_enable_dns_support                             = true
+  default_vpc_enable_dns_hostnames                           = true
   enable_nat_gateway                                         = true
-  enable_vpn_gateway                                         = true
+  enable_vpn_gateway                                         = false
   single_nat_gateway                                         = false
   one_nat_gateway_per_az                                     = true
   public_dedicated_network_acl                               = true
@@ -26,18 +27,42 @@ module "vpc" { #https://github.com/terraform-aws-modules/terraform-aws-vpc
   public_subnet_enable_resource_name_dns_a_record_on_launch  = true
   private_subnet_enable_resource_name_dns_a_record_on_launch = true
 
-  public_inbound_acl_rules = var.public_inbound_acl_rules
-  public_outbound_acl_rules = var.public_outbound_acl_rules
-  public_acl_tags = var.desc_tags
-  private_inbound_acl_rules = var.private_inbound_acl_rules
+  public_inbound_acl_rules   = var.public_inbound_acl_rules
+  public_outbound_acl_rules  = var.public_outbound_acl_rules
+  public_acl_tags            = var.desc_tags
+  private_inbound_acl_rules  = var.private_inbound_acl_rules
   private_outbound_acl_rules = var.private_outbound_acl_rules
-  private_acl_tags = var.desc_tags
-  tags = var.desc_tags
+  private_acl_tags           = var.desc_tags
+  tags                       = var.desc_tags
+}
+
+module "sg_ec2_bastion_host" { #https://github.com/terraform-aws-modules/terraform-aws-security-group
+  source = "./modules/sg"
+
+  name        = "${var.desc_tags.project}-sg-${var.sg1_name}"
+  description = var.sg1_description
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks      = [module.vpc.vpc_cidr_block]
+  ingress_with_cidr_blocks = var.sg1_ingress_with_cidr_blocks1
+  egress_with_cidr_blocks  = var.sg1_egress_with_cidr_blocks1
 }
 
 
 
+module "ec2_instance" { #https://github.com/terraform-aws-modules/terraform-aws-ec2-instance
+  source                 = "./modules/ec2"
+  vpc_security_group_ids = [module.sg_ec2_bastion_host.security_group_id]
+  name                   = "${var.desc_tags.project}-${var.ec2_name}"
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  monitoring             = false
+  subnet_id              = module.vpc.public_subnets[0]
+  iam_instance_profile   = var.iam_instance_profile
+  user_data              = base64encode(var.user_data)
 
+  tags = var.desc_tags
+}
 
 /*
 module "vpc_1" { #https://github.com/terraform-aws-modules/terraform-aws-vpc
--- Semana-Conectividade-e-Redes/Desafio4/variables.tf
+++ Semana-Conectividade-e-Redes/Desafio4/variables.tf
@@ -25,6 +25,7 @@ variable "domain" {
   default     = null
 }
 
+#VPC
 variable "vpc_id" {
   type        = string
   description = "VPC ID"
@@ -85,6 +86,61 @@ variable "private_outbound_acl_rules" {
   default     = null
 }
 
+#EC2
+variable "ec2_name" {
+  type        = string
+  description = "EC2 name"
+  default     = null
+}
+variable "instance_type" {
+  type        = string
+  description = "EC2 instance_type"
+  default     = "t2.micro"
+}
+variable "key_name" {
+  type        = string
+  description = "EC2 key_name"
+  default     = null
+}
+variable "iam_instance_profile" {
+  type        = string
+  description = "EC2 iam_instance_profile"
+  default     = null
+}
+variable "user_data" {
+  type        = string
+  description = "EC2 user_data"
+  default     = null
+}
+#variable "vpc_security_group_ids" {
+#  type        = list(string)
+#  description = "EC2 vpc_security_group_ids"
+#  default     = null
+#}
+
+
+
+#Security Groups
+variable "sg1_name" {
+  type        = string
+  description = "sg1_name"
+  default     = null
+}
+variable "sg1_description" {
+  type        = string
+  description = "sg1_description"
+  default     = null
+}
+variable "sg1_ingress_with_cidr_blocks1" {
+  type        = list(map(string))
+  description = "sg1_ingress_with_cidr_blocks1"
+  default     = null
+}
+variable "sg1_egress_with_cidr_blocks1" {
+  type        = list(map(string))
+  description = "sg1_egress_with_cidr_blocks1"
+  default     = null
+}
 
 
 /*
